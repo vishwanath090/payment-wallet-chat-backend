eda31c6ad2e6cf859b170862155f5cbd5f31f24d
vishwanath090 <vishwanathsbiradar1@gmail.com>
2025-11-24 23:18:36 +0530

    Fix transaction schema breaking deploy

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,13 +1,10 @@
 from pydantic import BaseModel
 from uuid import UUID
 from decimal import Decimal
-from enum import Enum
-from app.models.transaction import Transaction, TransactionType,TransactionStatus
-from typing import List
-class TransactionType(str, Enum):
-    ADD_MONEY = "ADD_MONEY"
-    TRANSFER = "TRANSFER"
-    RECEIVE = "RECEIVE"
+from typing import Optional, List
+
+# Use enums ONLY from models
+from app.models.transaction import TransactionType, TransactionStatus
 
 
 class TransactionRead(BaseModel):
@@ -15,17 +12,19 @@ class TransactionRead(BaseModel):
     wallet_id: UUID
     amount: Decimal
     type: TransactionType
-    status: TransactionStatus = TransactionStatus
+    status: TransactionStatus
 
-    provider_tx_id: str | None = None
-    counterparty: str | None = None
+    provider_tx_id: Optional[str] = None
+    counterparty: Optional[str] = None
 
     model_config = {"from_attributes": True}
+
+
 class TransactionHistoryResponse(BaseModel):
     page: int
     limit: int
-    total_items: int
+    total: int
     total_pages: int
-    next_page: int | None
-    prev_page: int | None
-    items: List[TransactionRead]
+    next_page: Optional[int]
+    prev_page: Optional[int]
+    data: List[TransactionRead]
